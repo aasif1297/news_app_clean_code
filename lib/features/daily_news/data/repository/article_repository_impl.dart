062354122988324287eca667f8c9ba8c91504fee
aasif1297 <aasif1297@gmail.com>
2024-03-11 15:07:58 +0100

    added article repo and impl

---
+++ lib/features/daily_news/data/repository/article_repository_impl.dart
@@ -0,0 +1,65 @@
+import 'dart:io';
+
+import 'package:clean_code_practice/core/resources/data_state.dart';
+import 'package:clean_code_practice/features/daily_news/data/data_sources/news_api_service.dart';
+import 'package:clean_code_practice/features/daily_news/data/models/article.dart';
+import 'package:clean_code_practice/features/daily_news/domain/entities/article.dart';
+import 'package:clean_code_practice/features/daily_news/domain/repository/article_repository.dart';
+import 'package:dio/dio.dart';
+
+import '../../../../core/constants/constants.dart';
+
+class ArticleRepositoryImpl extends ArticleRepository {
+  final NewsApiService _newsApiService;
+  ArticleRepositoryImpl(this._newsApiService);
+  @override
+  Future<DataState<List<ArticleModel>>> getNewsArticles() async {
+    try {
+      final httpResponse = await _newsApiService.getNewsArticles(
+        apiKey: newsApiKey,
+        country: countryQuery,
+        category: categoryQuery,
+      );
+
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        return DataFailure(
+          DioException(
+            requestOptions: httpResponse.response.requestOptions,
+            response: httpResponse.response,
+            error: httpResponse.response.statusMessage,
+            type: DioExceptionType.badResponse,
+          ),
+        );
+      }
+    } on DioException catch (e) {
+      if (e.type == DioExceptionType.connectionTimeout) {
+        return DataFailure(
+          DioException(
+            requestOptions: e.requestOptions,
+            type: DioExceptionType.connectionTimeout,
+            error: e.error,
+          ),
+        );
+      } else if (e.type == DioExceptionType.sendTimeout) {
+        return DataFailure(
+          DioException(
+            requestOptions: e.requestOptions,
+            type: DioExceptionType.sendTimeout,
+          ),
+        );
+      } else if (e.type == DioExceptionType.receiveTimeout) {
+        return DataFailure(
+          DioException(
+            requestOptions: e.requestOptions,
+            type: DioExceptionType.receiveTimeout,
+            error: e.error,
+          ),
+        );
+      } else {
+        return DataFailure(e);
+      }
+    }
+  }
+}
